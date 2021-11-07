026fb1c6d5e94bc2af497c45f5a05ca4027d9b34
Logan Kulinski <lbkulinski@icloud.com>
2021-11-07 12:28:29 -0600

    Menu parsing updates

--- src/main/java/com/zuppler4j/menu/Category.java
+++ src/main/java/com/zuppler4j/menu/Category.java
@@ -2,6 +2,8 @@ package com.zuppler4j.menu;
 
 import java.util.List;
 import com.zuppler4j.Image;
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * A category of the Zuppler API.
@@ -20,4 +22,31 @@ import com.zuppler4j.Image;
  */
 public record Category(Integer id, String name, String description, List<MenuItem> items, Boolean active, Image image,
                        Integer minOrderQty, Integer priority, List<String> tags) {
+    /**
+     * Constructs a newly allocated {@code Category} object with the specified ID, name, description, {@code List} of
+     * {@code MenuItem}s, active flag, {@code Image}, minimum order quantity, priority, and {@code List} of tags.
+     *
+     * @param id the ID to be used in construction
+     * @param name the name to be used in construction
+     * @param description the description to be used in construction
+     * @param items the {@code List} of {@code MenuItem}s to be used in construction
+     * @param active the active flag to be used in construction
+     * @param image the {@code Image} to be used in construction
+     * @param minOrderQty the minimum order quantity to be used in construction
+     * @param priority the priority to be used in construction
+     * @param tags the {@code List} of tags to be used in construction
+     */
+    public Category {
+        if (items != null) {
+            items = new ArrayList<>(items);
+
+            items = Collections.unmodifiableList(items);
+        } //end if
+
+        if (tags != null) {
+            tags = new ArrayList<>(tags);
+
+            tags = Collections.unmodifiableList(tags);
+        } //end if
+    } //Category
 }
