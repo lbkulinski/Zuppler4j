879820bbe2a685711fe2633b6c6c3bd67006e348
Logan Kulinski <lbkulinski@users.noreply.github.com>
2021-11-07 12:44:04 -0600

    Menu parsing updates

--- src/main/java/com/zuppler4j/menu/ItemModifier.java
+++ src/main/java/com/zuppler4j/menu/ItemModifier.java
@@ -1,12 +1,14 @@
 package com.zuppler4j.menu;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * An item modifier of the Zuppler API.
  *
  * @author Logan Kulinski, lbkulinski@icloud.com
- * @version November 6, 2021
+ * @version November 7, 2021
  * @param id the ID of this item modifier
  * @param name the name of this item modifier
  * @param description the description of this item modifier
@@ -27,4 +29,36 @@ public record ItemModifier(Integer id, String name, String description, List<Ite
                            Boolean allowGrouping, List<String> dependsOn, Integer divider, Integer masterModifierId,
                            Integer minSelections, Integer maxSelections, Boolean modifierQuantity,
                            Boolean multipleModifiers, Boolean multipleSelections, Integer priority) {
+    /**
+     * Constructs an instance of the {@code ItemModifier} class.
+     *
+     * @param id the ID to be used in construction
+     * @param name the name to be used in construction
+     * @param description the description to be used in construction
+     * @param options the {@code List} of {@code ItemOption}s to be used in construction
+     * @param active the active flag to be used in construction
+     * @param allowGrouping the flag to allow grouping to be used in construction
+     * @param dependsOn the {@code List} of dependents to be used in construction
+     * @param divider the divider to be used in construction
+     * @param masterModifierId the master modifier ID to be used in construction
+     * @param minSelections the minimum selections to be used in construction
+     * @param maxSelections the maximum selections to be used in construction
+     * @param modifierQuantity the modifier quantity to be used in construction
+     * @param multipleModifiers the multiple modifiers flag to be used in construction
+     * @param multipleSelections the multiple selections flag to be used in construction
+     * @param priority the priority to be used in construction
+     */
+    public ItemModifier {
+        if (options != null) {
+            options = new ArrayList<>(options);
+
+            options = Collections.unmodifiableList(options);
+        } //end if
+
+        if (dependsOn != null) {
+            dependsOn = new ArrayList<>(dependsOn);
+
+            dependsOn = Collections.unmodifiableList(dependsOn);
+        } //end if
+    } //ItemModifier
 }
