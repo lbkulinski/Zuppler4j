7417ac5baa5d7a6b3d2c0a3e43dc11793d9c1352
logankulinski <logan.kulinski@realtyads.com>
2021-11-19 15:52:59 -0600

    Type adapter updates

--- src/main/java/com/zuppler4j/adapters/TimeAvailabilityAdapter.java
+++ src/main/java/com/zuppler4j/adapters/TimeAvailabilityAdapter.java
@@ -4,6 +4,7 @@ import com.google.gson.TypeAdapter;
 import com.zuppler4j.TimeAvailability;
 import com.google.gson.stream.JsonWriter;
 import java.io.IOException;
+import java.util.Objects;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonToken;
 
@@ -20,9 +21,14 @@ public final class TimeAvailabilityAdapter extends TypeAdapter<TimeAvailability>
      * @param jsonWriter the {@link JsonWriter} to be used in the operation
      * @param timeAvailability the {@link TimeAvailability} to be used in the operation
      * @throws IOException if an I/O error occurs
+     * @throws NullPointerException if the specified {@link JsonWriter} or {@link TimeAvailability} is {@code null}
      */
     @Override
     public void write(JsonWriter jsonWriter, TimeAvailability timeAvailability) throws IOException {
+        Objects.requireNonNull(jsonWriter, "the specified JsonWriter is null");
+
+        Objects.requireNonNull(timeAvailability, "the specified TimeAvailability is null");
+
         jsonWriter.beginObject();
 
         jsonWriter.name("open");
@@ -42,9 +48,12 @@ public final class TimeAvailabilityAdapter extends TypeAdapter<TimeAvailability>
      * @param jsonReader the {@link JsonReader} to be used in the operation
      * @return the deserialized {@link TimeAvailability} object
      * @throws IOException if an I/O error occurs
+     * @throws NullPointerException if the specified {@link JsonReader} is {@code null}
      */
     @Override
     public TimeAvailability read(JsonReader jsonReader) throws IOException {
+        Objects.requireNonNull(jsonReader, "the specified JsonReader is null");
+
         jsonReader.beginObject();
 
         Integer open = null;
